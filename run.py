7aa1b8030fde5e6c5153426f173b07a76d4e36f6
Lister223 <100354916+Lister223@users.noreply.github.com>
2024-01-13 16:25:49 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import model
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template ,redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
